414fee540877e880e45b118aa0aaf3ae1bc66d80
andreaconv <andrea2convertini@gmail.com>
2023-10-17 17:21:29 +0200

    [edit] ottenere la lista di pizze (filtrare per titolo)

--- src/main/java/org/java/app/api/controller/PizzaApiController.java
+++ src/main/java/org/java/app/api/controller/PizzaApiController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,9 +22,16 @@ public class PizzaApiController {
 	private PizzaService pizzaService;
 	
 	@GetMapping
-	public ResponseEntity<List<Pizza>> getAllPizze(){
+	public ResponseEntity<List<Pizza>> getAllPizze(
+			@RequestParam(required = false, name = "q") String query
+			){
 		
 		List<Pizza> pizze = pizzaService.findAll();
+		
+		if (query == null) 
+			pizze = pizzaService.findAll();
+		else 
+			pizze = pizzaService.findByNome(query);
 		
 		return new ResponseEntity<>(pizze, HttpStatus.OK);
 	}
