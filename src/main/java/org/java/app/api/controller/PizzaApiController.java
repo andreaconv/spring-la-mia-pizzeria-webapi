3188f5a9a2c7cab801d468dbe89852d21364fe50
andreaconv <andrea2convertini@gmail.com>
2023-10-17 17:30:30 +0200

    [edit] leggere i dettagli di una singola pizza in base all'ID

--- src/main/java/org/java/app/api/controller/PizzaApiController.java
+++ src/main/java/org/java/app/api/controller/PizzaApiController.java
@@ -1,6 +1,7 @@
 package org.java.app.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.java.app.db.pojo.Pizza;
 import org.java.app.db.serv.PizzaService;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,21 @@ public class PizzaApiController {
 			pizze = pizzaService.findByNome(query);
 		
 		return new ResponseEntity<>(pizze, HttpStatus.OK);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Pizza> getPizzaById(
+			@PathVariable int id
+		) {
+		
+		Optional<Pizza> optPizza = Optional.ofNullable(pizzaService.findById(id));
+		
+		if (optPizza.isEmpty()) {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<>(optPizza.get(), HttpStatus.OK);
 	}
 
 }
