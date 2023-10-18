aa55a247a48a4455900f46e9c71e442be5382e75
andreaconv <andrea2convertini@gmail.com>
2023-10-18 17:00:46 +0200

    [ADD] chiamata axios di tutte le pizze in console

--- frontend/api-vue-frontend/src/views/HomeView.vue
+++ frontend/api-vue-frontend/src/views/HomeView.vue
@@ -1,4 +1,21 @@
 <script setup>
+//si usa ref bene o male come i data
+import { onMounted, ref } from 'vue';
+import axios from "axios";
+const API_URL = "http://localhost:8080/api/v1.0";
+
+onMounted(() => {
+
+  axios.get(API_URL + "/pizze")
+        .then(res => {
+
+          const data = res.data;
+          console.log("axios data: " + JSON.stringify(data, null, 2));
+      })
+      .catch(err => console.log(err));
+
+})
+
 </script>
 
 <template>
