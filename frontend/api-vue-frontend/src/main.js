24371f6d1eb745e9b502377f64f3ecc3a29b821b
andreaconv <andrea2convertini@gmail.com>
2023-10-19 19:19:04 +0200

    [add] import BOOTSTRAP

--- frontend/api-vue-frontend/src/main.js
+++ frontend/api-vue-frontend/src/main.js
@@ -1,5 +1,7 @@
 // importo lo style col File.css
 import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
